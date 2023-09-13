6e44708bba3657edebb6d7f605c631770c5ef81d
shenghui <shenghui0779@gmail.com>
2023-09-13 10:33:35 +0800

    Update value.go

--- value_test.go
+++ value_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestM(t *testing.T) {
+func TestV(t *testing.T) {
 	v1 := V{}
 
 	v1.Set("bar", "baz")
@@ -35,24 +35,4 @@ func TestM(t *testing.T) {
 	assert.Equal(t, "bar=baz&hello=world", v3.Encode("=", "&", WithEmptyEncMode(EmptyEncIgnore)))
 	assert.Equal(t, "bar=baz&foo=", v3.Encode("=", "&", WithIgnoreKeys("hello")))
 	assert.Equal(t, "bar=baz", v3.Encode("=", "&", WithIgnoreKeys("hello"), WithEmptyEncMode(EmptyEncIgnore)))
-}
-
-func TestXML(t *testing.T) {
-	m := V{
-		"appid":     "wx2421b1c4370ec43b",
-		"partnerid": "10000100",
-		"prepayid":  "WX1217752501201407033233368018",
-		"package":   "Sign=WXPay",
-		"noncestr":  "5K8264ILTKCH16CQ2502SI8ZNMTM67VS",
-		"timestamp": "1514363815",
-	}
-
-	x, err := FormatVToXML(m)
-
-	assert.Nil(t, err)
-
-	r, err := ParseXMLToV([]byte(x))
-
-	assert.Nil(t, err)
-	assert.Equal(t, m, r)
 }
